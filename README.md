f103078d68447776e3b2db84a330877821fcc9bf
Dennis Ploeger <develop@dieploegers.de>
2022-05-05 12:37:33 +0200

    feat: Moved to nsenter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # scalyr-k8snode-manager
 
-[![Artifact HUB](https://img.shields.io/endpoint?url=https://artifacthub.io/badge/repository/scalyr-k8snode-manager)](https://artifacthub.io/packages/search?repo=scalyr-k8snode-manager) ![Version: 0.1.1](https://img.shields.io/badge/Version-0.1.1-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: latest](https://img.shields.io/badge/AppVersion-latest-informational?style=flat-square)
+[![Artifact HUB](https://img.shields.io/endpoint?url=https://artifacthub.io/badge/repository/scalyr-k8snode-manager)](https://artifacthub.io/packages/search?repo=scalyr-k8snode-manager) ![Version: 0.1.2](https://img.shields.io/badge/Version-0.1.2-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: latest](https://img.shields.io/badge/AppVersion-latest-informational?style=flat-square)
 
 ## Introduction
 
@@ -22,10 +22,13 @@ to install this chart.
 
 Theses basic configuration keys have to be set up for managing Scalyr agents on the Kubernetes nodes:
 
+* config.method: Use nsenter (via kubectl node-shell) or ssh to get access to the node [nsenter]
 * config.sshKey: The private key data of the SSH key used to connect to the Kubernetes nodes. Be sure that the
-    public part of that keypair is allowed to connect to the node.
+    public part of that keypair is allowed to connect to the node. (only for config.method = ssh)
 * config.sshPassPhrase: The passphrase of the private key used to connect to the Kubernetes nodes.
+    (only for config.method = ssh)
 * config.sshUser: The remote user connecting to the Kubernetes nodes (e.g. ec2-user on EKS managed nodegroups)
+    (only for config.method = ssh)
 * env: Additional environment variables (useful for templating; see below)
 * config.scalyr.server: The name of the Scalyr api server (defaults to scalyr.com)
 * config.scalyr.apiKey: The api key used to authenticate to the Scalyr api server
@@ -44,15 +47,16 @@ for each monitor.
 | affinity | object | `{}` |  |
 | config.cronSchedule | string | `"0 0 * * *"` | Schedule to use for the CronJob |
 | config.env | list | `[]` | Additional environment variables to set in the PodSpec Env-form (list of objects with name and value keys) |
+| config.method | string | `"nsenter"` | Use nsenter (via kubectl node-shell) or ssh to get access to the node |
 | config.scalyr.apiKey | string | `""` | The Scalyr API key to use |
 | config.scalyr.base64Config | bool | `true` | As Helm is currently [unable to correctly pass JSON strings](https://github.com/helm/helm/issues/5618), this can be set to true so all values of scalyr.config are expected to be base64 encoded and will be decoded in the chart |
 | config.scalyr.config | object | `{}` | A hash of configuration files and their content as documented in the [Scalyr agent configmap configuration documentation](https://app.scalyr.com/help/scalyr-agent-k8s#modify-config) |
 | config.scalyr.server | string | `"scalyr.com"` | The Scalyr server to send logs to |
 | config.scalyr.version | string | `""` | Version of Scalyr to use |
-| config.sftpServer | string | `"/usr/libexec/openssh/sftp-server"` | Path to the SFTP-Server binary on the node |
-| config.sshKey | string | `""` | SSH private key data to use |
-| config.sshPassPhrase | string | `""` | SSH passphrase for the key |
-| config.sshUser | string | `""` | User to use when connecting to the node |
+| config.sftpServer | string | `"/usr/libexec/openssh/sftp-server"` | Path to the SFTP-Server binary on the node (only for config.method = ssh) |
+| config.sshKey | string | `""` | SSH private key data to use (only for config.method = ssh) |
+| config.sshPassPhrase | string | `""` | SSH passphrase for the key (only for config.method = ssh) |
+| config.sshUser | string | `""` | User to use when connecting to the node (only for config.method = ssh) |
 | fullnameOverride | string | `""` |  |
 | image.pullPolicy | string | `"Always"` |  |
 | image.repository | string | `"dodevops/scalyr-k8snode-manager"` |  |
--- manage.sh
+++ manage.sh
@@ -3,6 +3,7 @@
 # Manages Scalyr agent installation on k8s nodes
 # Supported environment variables:
 #
+# METHOD=<nsenter or ssh>
 # SCALYR_VERSION=<valid scalyr agent version>
 # SCALYR_SERVER=<Scalyr server to use>
 # SCALYR_APIKEY=<Scalyr API key>
@@ -12,41 +13,46 @@
 # SSH_USER=<Username to use for connecting to the node>
 # SFTP_SERVER=Path to the sftp-server executable on the node [/usr/libexec/openssh/sftp-server]
 #
-# The nodes will be accessed using SSH, so a valid SSH private key should be mounted into the
+# If the nodes will be accessed using SSH, a valid SSH private key should be mounted into the
 # container.
 #
 # All configuration files will be preprocessed using [Mo](https://github.com/tests-always-included/mo), so all
 # environment variables will be available as a template context.
 # Additionally, the environment variable "NODE" contains the current node
 
-rm -rf ~/.ssh &>/dev/null
-mkdir ~/.ssh
-chmod 0700 ~/.ssh
-cat "${SSH_KEY_PATH}" > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-echo "#!/usr/bin/env sh" > /usr/local/bin/sshkey
-echo "echo '${SSH_PASSPHRASE}'" >> /usr/local/bin/sshkey
-chmod +x /usr/local/bin/sshkey
-export SSH_ASKPASS=/usr/local/bin/sshkey
+COMMAND="kubectl node-shell"
+if [[ "X${METHOD}X" == "XsshX" ]]
+then
+  COMMAND="ssh"
+  rm -rf ~/.ssh &>/dev/null
+  mkdir ~/.ssh
+  chmod 0700 ~/.ssh
+  cat "${SSH_KEY_PATH}" > ~/.ssh/id_rsa
+  chmod 0600 ~/.ssh/id_rsa
+  echo "#!/usr/bin/env sh" > /usr/local/bin/sshkey
+  echo "echo '${SSH_PASSPHRASE}'" >> /usr/local/bin/sshkey
+  chmod +x /usr/local/bin/sshkey
+  export SSH_ASKPASS=/usr/local/bin/sshkey
 
-# use DISPLAY and setsid to work around ssh scripting limitations
-# (https://unix.stackexchange.com/questions/83986/tell-ssh-to-use-a-graphical-prompt-for-key-passphrase)
-export DISPLAY=:0.0
-setsid ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+  # use DISPLAY and setsid to work around ssh scripting limitations
+  # (https://unix.stackexchange.com/questions/83986/tell-ssh-to-use-a-graphical-prompt-for-key-passphrase)
+  export DISPLAY=:0.0
+  setsid ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 
-echo -e "Host *\n  User ${SSH_USER}\n  UserKnownHostsFile=/dev/null\n  StrictHostKeyChecking=no\n" > /root/.ssh/config;
+  echo -e "Host *\n  User ${SSH_USER}\n  UserKnownHostsFile=/dev/null\n  StrictHostKeyChecking=no\n" > /root/.ssh/config;
+fi
 
 for NODE in $(kubectl get nodes -o custom-columns=name:metadata.name --no-headers)
 do
   echo "Managing node ${NODE}"
   echo "Downloading Scalyr agent installer"
-  if ! OUTPUT=$(setsid ssh "${NODE}" -- "sudo curl -sO https://www.scalyr.com/install-agent.sh" 2>&1)
+  if ! OUTPUT=$(setsid "${COMMAND}" "${NODE}" -- "sudo curl -sO https://www.scalyr.com/install-agent.sh" 2>&1)
   then
     echo -e "Can't download Scalyr installer:\n ${OUTPUT}"
     exit 1
   fi
   echo "Installing Scalyr"
-  if ! OUTPUT=$(setsid ssh "${NODE}" -- "sudo bash install-agent.sh --set-api-key '${SCALYR_APIKEY}' --version '${SCALYR_VERSION}' --set-scalyr-server '${SCALYR_SERVER}'" 2>&1)
+  if ! OUTPUT=$(setsid "${COMMAND}" "${NODE}" -- "sudo bash install-agent.sh --set-api-key '${SCALYR_APIKEY}' --version '${SCALYR_VERSION}' --set-scalyr-server '${SCALYR_SERVER}'" 2>&1)
   then
     echo -e "Can't install Scalyr:\n ${OUTPUT}"
     exit 1
@@ -63,15 +69,24 @@ do
       exit 1
     fi
     echo "Uploading configuration ${CONFIGFILE}"
-    if ! OUTPUT=$(echo "put \"${TEMPDIR}/${CONFIGFILE}\" /etc/scalyr-agent-2/agent.d" | setsid sftp -s "sudo -u root ${SFTP_SERVER:-/usr/libexec/openssh/sftp-server}" "${NODE}" 2>&1)
+    if [[ "X${METHOD}X" == "XsshX" ]]
     then
-      echo -e "Can't copy configuration:\n ${OUTPUT}"
-      exit 1
+      if ! OUTPUT=$(echo "put \"${TEMPDIR}/${CONFIGFILE}\" /etc/scalyr-agent-2/agent.d" | setsid sftp -s "sudo -u root ${SFTP_SERVER:-/usr/libexec/openssh/sftp-server}" "${NODE}" 2>&1)
+      then
+        echo -e "Can't copy configuration:\n ${OUTPUT}"
+        exit 1
+      fi
+    else
+      if ! OUTPUT=$(kubectl node-shell "${NODE}" -- sh -c 'cat > /etc/scalyr-agent-2/agent.d' <"${TEMPDIR}/${CONFIGFILE}" 2>&1)
+      then
+        echo -e "Can't copy configuration:\n ${OUTPUT}"
+        exit 1
+      fi
     fi
   done
   rm -rf "${TEMPDIR}"
   echo "Starting Scalyr Agent"
-  if ! OUTPUT=$(setsid ssh "${NODE}" -- "sudo systemctl restart scalyr-agent-2" 2>&1)
+  if ! OUTPUT=$(setsid "${COMMAND}" "${NODE}" -- "sudo systemctl restart scalyr-agent-2" 2>&1)
   then
     echo -e "Can't start scalyr agent:\n ${OUTPUT}"
     exit 1
